c9fcd9dfebde6940bd380e4bbb9333634ed3f8f4
Aceaa28 <ahernandez3@aoiths.org>
2023-02-08 12:44:16 -0500

    save

--- Assets/Scripts/GameInstructions.cs
+++ Assets/Scripts/GameInstructions.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class GameInstructions : MonoBehaviour
 {
-    string gameInstructions = "Move the player by using the W,A,S and D keys";
+     public string gameInstructions = "Move the player by using the W,A,S and D keys";
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log();
+        Debug.Log(gameInstructions);
     }
 
     // Update is called once per frame
